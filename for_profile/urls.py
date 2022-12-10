e35e1d92aaf8217d1bd68638cdd043cc098bd7a4
rathinashabur <rathina.shabur@ui.ac.id>
2022-12-10 15:32:41 +0700

    tambah views show flutter

--- for_profile/urls.py
+++ for_profile/urls.py
@@ -6,4 +6,6 @@ app_name = 'for_profile'
 urlpatterns = [
     path('show_profile/', show_profile, name = 'show_profile'),
     path('edit_profile/', edit_profile, name = 'edit_profile'),
+    path('show_profile_flutter/', show_profile_flutter, name = 'show_profile_flutter'),
+    path('edit_profile_flutter/', edit_profile_flutter, name = 'edit_profile_flutter'),
 ]
