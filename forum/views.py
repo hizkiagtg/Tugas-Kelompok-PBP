2509e77050ec0dd169fccad559328d94e105da28
Hizkia Sebastian Ginting <92731992+hizkiagtg@users.noreply.github.com>
2022-12-10 21:43:01 +0700

    Update views.py

--- forum/views.py
+++ forum/views.py
@@ -3,20 +3,34 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Question, Answer
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponseNotFound
-from datetime import datetime
+from accounts.models import *
+import datetime
+import json
 
 @login_required(login_url='accounts:login')
 def addQuestion(request):
     if request.method == "POST":
         title = request.POST.get('title')
         body = request.POST.get('description')
-        newQuestion = Question(author=request.user, title=title, body = body, created_at= datetime.now())
+        newQuestion = Question(author=request.user, title=title, body = body, created_at= datetime.date.today())
         newQuestion.save()
         return HttpResponse(serializers.serialize("json",[newQuestion]), content_type="application/json")
     return HttpResponseNotFound()
 
+@csrf_exempt
+def addFlutter(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        user = User.objects.get(id=data['user'])
+        title = data['title']
+        body = data['body']
+        newQuestion = Question(author = user, title=title, body = body, created_at= datetime.date.today())
+        newQuestion.save()
+        return JsonResponse({"instance": "Forum Berhasil Dibuat!"}, status=200)
+    
 @login_required(login_url='accounts:login')
 def addAnswer(request):
     if request.method == "POST":
