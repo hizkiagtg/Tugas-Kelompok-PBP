7331c42afe3e24a9ec0007955c5d6456f4aac538
rathinashabur <rathina.shabur@ui.ac.id>
2022-12-11 18:55:25 +0700

    views profile

--- for_profile/views.py
+++ for_profile/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, JsonResponse
 from .forms import *
 from accounts.models import User
 import json
@@ -27,9 +27,9 @@ def edit_profile(request):
             email_exists = User.objects.filter(email=request.POST.get("email")).exists()
             username_exists = User.objects.filter(username=request.POST.get("username")).exists()
 
-            if '@' in request.POST.get("username"):
-                messages.error(request, "Username can't contain @")
-            elif email_exists:
+            # if '@' in request.POST.get("username"):
+            #     messages.error(request, "Username can't contain @")
+            if email_exists:
                 messages.error(request, "Email is already used! Use other email.")
             elif username_exists:
                 messages.error(request, "Username is already used! Choose other username.")
@@ -80,55 +80,128 @@ def user_json_flutter(request, id_user):
     user = User.objects.get(id=id_user)
     return HttpResponse(serializers.serialize('json', user), content_type='application/json')
 
+
 @csrf_exempt
-def edit_profile_flutter(request):   
-    if request.method == 'POST':
-        editedProfile = json.loads(request.body)
-        user = User.objects.get(id=editedProfile['id'])
-        form_edit = None
-        if user.is_regular:
-            if request.method == 'POST':
-                form_edit = EditProfileFormReg(request.POST, instance=user)
-                email_exists = User.objects.filter(email=request.POST.get("email")).exists()
-                username_exists = User.objects.filter(username=request.POST.get("username")).exists()
+def edit_reg_flutter(request):
+	data = {}
 
-                if '@' in request.POST.get("username"):
-                    messages.error(request, "Username can't contain @")
-                elif email_exists:
-                    messages.error(request, "Email is already used! Use other email.")
-                elif username_exists:
-                    messages.error(request, "Username is already used! Choose other username.")
-                elif form_edit.is_valid():
-                    form_edit.save()
-                    messages.success(request, "Succesfully Updated Profile!")
-                    return redirect('for_profile:show_profile')
-                else:
-                    form_edit = EditProfileFormReg(request.POST, instance=user,
-                    initial = {
-                    "username": form_edit.username,
-                    "email": form_edit.email,
-                    "name": form_edit.name,
-                    "age": form_edit.age,
-                    "gender": form_edit.gender,
-                    "city": form_edit.city,
-                    })
+	if request.method == 'POST':
+		form = EditProfileFormReg(request.POST)
+		
+		username_exists = User.objects.filter(username=request.POST.get("username")).exists()
+		email_exists = User.objects.filter(email=request.POST.get("email")).exists()
 
-        elif user.is_bank:
-            if request.method == 'POST':
-                form_edit = EditProfileFormBank(request.POST, instance=user)
-                email_exists = User.objects.filter(email=request.POST.get("email")).exists()
+		if '@' in request.POST.get("username"):
+			data['success'] = False
+			data['warning'] = "Username can not contain @."
+			return JsonResponse({'data': data}, status=401)
+		
+		if username_exists:
+			data['success'] = False
+			data['warning'] = "Username has already been used."
+			return JsonResponse({'data': data}, status=401)
+			
+		elif email_exists:
+			data['success'] = False
+			data['warning'] = "Email has already been used."
+			return JsonResponse({'data': data}, status=401)
+        
+		else:
+			if form.is_valid():
+				data['success'] = True
+				user = form.save(commit=False)
+				user.is_regular = True
+				user.save()
+				return JsonResponse({'data': data}, status=200)
+			
+			else:
+				data['success'] = False
+				req = False
+				message = ""
 
-                if email_exists:
-                    messages.error(request, "Email is already used! Use other email.")
-                elif form_edit.is_valid():
-                    form_edit.save()
-                    messages.success(request, "Succesfully Updated Profile!")
-                    return redirect('for_profile:show_profile')
-                else:
-                    form_edit = EditProfileFormBank(request.POST, instance=user,
-                    initial = {
-                    "name": form_edit.name,
-                    "email": form_edit.email,
-                    "city": form_edit.city,
-                    "address": form_edit.address,
-                })
+				for msg in form.errors: 
+					if (form.errors[msg] == "This field is required."): 
+						req = True
+					else: 
+						message += (form.errors[msg] + "\n")
+
+				if (req == True) :
+					message = "All fields are required to be filled."
+				else :
+					message = str(message).replace(".", ".\n")
+					message = str(message).replace('<ul class="errorlist">', "")
+					message = str(message).replace('<li>', "")
+					message = str(message).replace('</li>', "")
+					message = str(message).replace('</ul>', "")
+					# message = newMessage
+                
+				data['warning'] = message
+				context = {"form": form}
+
+			return JsonResponse({'data': data}, status=401)
+
+
+	form = EditProfileFormReg()
+	data['success'] = False
+	data['warning'] = 'Please try again.'
+	return JsonResponse({'data': data}, status=401)
+
+
+@csrf_exempt
+def edit_bank_flutter(request):
+	data = {}
+
+	if request.method == 'POST':
+		form = EditProfileFormBank(request.POST)
+		
+		name_exists = User.objects.filter(is_bank=True, name=request.POST.get("name")).exists()
+		email_exists = User.objects.filter(email=request.POST.get("email")).exists()
+        
+		if name_exists:
+			data['success'] = False
+			data['warning'] = "Institute name has already been registered."
+			return JsonResponse({'data': data}, status=401)
+        
+		elif email_exists:
+			data['success'] = False
+			data['warning'] =  "Email has already been used."
+			return JsonResponse({'data': data}, status=401)
+        
+		else:
+			if form.is_valid():
+				data['success'] = True
+				user = form.save(commit=False)
+				user.is_bank = True
+				user.save()
+				return JsonResponse({'data': data}, status=200)
+			
+			else:
+				data['success'] = False
+				req = False
+				message = ""
+
+				for msg in form.errors: 
+					if (form.errors[msg] == "This field is required."): 
+						req = True
+					else: 
+						message += (form.errors[msg] + "\n")
+
+				if (req == True) :
+					message = "All fields are required to be filled."
+				else :
+					message = str(message).replace(".", ".\n")
+					message = str(message).replace('<ul class="errorlist">', "")
+					message = str(message).replace('<li>', "")
+					message = str(message).replace('</li>', "")
+					message = str(message).replace('</ul>', "")
+                
+				data['warning'] = message
+				context = {"form": form}
+
+			return JsonResponse({'data': data}, status=401)
+
+	form = EditProfileFormBank()
+	data['success'] = False
+	data['warning'] = 'Please try again.'
+	return JsonResponse({'data': data}, status=401)
+
